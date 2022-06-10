04fac67e7cb4c5a0a6b32f6aec09c12a327955b1
atks <adriantks@gmail.com>
2022-06-10 15:18:51 +0800

    gb-io working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate gb_io;
+
+use std::env;
+use std::fs::File;
+use std::io;
+use gb_io::reader::SeqReader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("printing {:?}", args);
+    let gb_file: String = args[1].clone();
+
+    println!("{:?}", gb_file);
+
+    let file = File::open(gb_file).unwrap();
+    let stdout = io::stdout();
+    for seq in SeqReader::new(file) {
+        let seq = seq.unwrap();
+        let rc = seq.revcomp();
+        rc.write(stdout.lock()).unwrap();
+
+        break;
+    }
+
+
+
 }
